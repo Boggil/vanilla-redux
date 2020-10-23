4ab9366ad3b46a8bc014140d0455b7a376062a5b
Bokgil <boggil3665@daum.net>
2020-10-23 11:39:50 +0900

    finish
    react로 redux를 사용해 간단한 todo 예제 끝.

---
+++ src/store.js
@@ -0,0 +1,41 @@
+// import { createStore } from 'redux';
+import { configureStore, createAction, createReducer, createSlice } from '@reduxjs/toolkit';
+
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
+
+// /**
+//  * createReducer는 Immer를 사용하므로 불변성을 관리해준다.
+//  * 그래서 push가 가능함.
+//  * */
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() }); // push is mutate state
+//   },
+//   [deleteToDo]: (state, action) =>
+//     state.filter(toDo => toDo.id !== action.payload)  // filter is return new array
+// });
+
+const toDos = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() }); // push is mutate state
+    },
+    remove: (state, action) =>
+      state.filter(toDo => toDo.id !== action.payload)
+  }
+});
+
+const store = configureStore({ reducer: toDos.reducer });
+// const store = configureStore({ reducer });
+
+export const { add, remove } = toDos.actions;
+
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo,
+// };
+
+export default store;
